c7fc804851d3ca2ad0974a554d418ba9f3704488
manasi <khilaremanasi41@gmail.com>
2023-08-30 01:47:18 +0530

    added comment

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-feature mde hi file ghatliye mi 
-steps git branch -> feature vr ahe ka te bghne -> new js file created ->git staus check
-->git add . -> git commit -m"msg"
+// feature mde hi file ghatliye mi 
+// steps git branch -> feature vr ahe ka te bghne -> new js file created ->git staus check
+// ->git add . -> git commit -m"msg"
